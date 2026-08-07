d981befebe362c52f9c47bd66b7f581070470079
jankases <surasak.jankasem@gmail.com>
2026-08-08 01:51:29 +0700

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,4 @@
+// assets/js/config.js
+export const SUPABASE_URL = 'https://trrfwszgscftnybhhsor.supabase.co'; 
+export const SUPABASE_KEY = 'sb_publishable_SyOGO6gN7DGYmj0ltcvokg_y3WOtZdH'; 
+export const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
