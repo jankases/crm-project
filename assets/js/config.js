005874ff3b6d28a69ec3770d86571ace3654e3d3
jankases <surasak.jankasem@gmail.com>
2026-08-08 02:02:54 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,10 @@
-// assets/js/config.js
-export const SUPABASE_URL = 'https://trrfwszgscftnybhhsor.supabase.co'; 
-export const SUPABASE_KEY = 'sb_publishable_SyOGO6gN7DGYmj0ltcvokg_y3WOtZdH'; 
-export const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// =========================================
+// CRM System - Global Configuration
+// =========================================
+
+// กุญแจสาธารณะ (Publishable Key) สำหรับฝั่ง Frontend ปลอดภัยที่จะเปิดเผย
+const SUPABASE_URL = 'https://trrfwszgscftnybhhsor.supabase.co'; 
+const SUPABASE_KEY = 'sb_publishable_SyOGO6gN7DGYmj0ltcvokg_y3WOtZdH'; 
+
+// สร้างตัวเชื่อมต่อ Database ให้ทุกหน้าจอสามารถเรียกใช้งานได้
+window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
